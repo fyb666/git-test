a9a343592916c71758886d9e8f840f7f3752a6a4
fyb <1986839291@qq.com>
2021-09-13 08:14:30 +0800

    push commit2

--- src/main/java/com/atgugui/git/GitTest.java
+++ src/main/java/com/atgugui/git/GitTest.java
@@ -6,8 +6,9 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
-        System.out.println("hello git5!");
+        System.out.println("hello gitx5!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
